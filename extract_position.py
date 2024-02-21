b8a5ab9ef947be358b7889bb26f3279790720c9f
Andrew Alsing <andrew32alsing@gmail.com>
2024-02-21 15:45:19 -0800

    completed exercise 2

--- extract_position.py
+++ extract_position.py
@@ -1,19 +1,23 @@
 def extract_position(line):
-    if not line:
-        pos = None
-    else:
-        if 'debug' in line or 'error' in line:
-            pos = None
-        else:
-            if 'x:' in line:
-                start_index = line.find('x:') + 2
-                pos = line[start_index:] # from start_index to the end.
-            else:
-                pos = None
-    return pos
+    if not line or 'debug' in line or 'error' in line:
+        raise ValueError("Invalid line")
+    
+    if 'x:' not in line:
+        raise ValueError("No position found in line")
+    
+    start_index = line.find('x:') + 2
+    pos = line[start_index:].strip()
+    return float(pos)
 
 if __name__ == "__main__":
-    result1 = extract_position('|error| numerical calculations could not converge.')
-    print(result1)
-    result2 = extract_position('|update| the positron location in the particle accelerator is x:21.432')
-    print(result2)
+    try:
+        result1 = extract_position('|error| numerical calculations could not converge.')
+        print(result1)
+    except ValueError as e:
+        print(e)
+
+    try:
+        result2 = extract_position('|update| the positron location in the particle accelerator is x:21.432')
+        print(result2)
+    except ValueError as e:
+        print(e)
